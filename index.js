f3bbd018632e95ad2b45a5a2fad88753a48cbd9c
areadne <115907950+areadne@users.noreply.github.com>
2022-10-30 13:29:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,70 @@
-let montoAhorro
-let cantidadTiempo
+const productos = [
+    {id: 1, producto: "labial en barra rojo", precio: 2500, stock: 10},
+    {id: 2, producto: "labial en barra burdeos", precio: 2500, stock: 2},
+    {id: 3, producto: "labial en barra rosa", precio: 2500, stock: 12},
+    {id: 4, producto: "labial liquido durazno", precio: 3500, stock: 2},
+    {id: 5, producto: "labial liquido rojo", precio: 3500, stock: 8},
+    {id: 6, producto: "labial liquido rosa", precio: 3500, stock: 0},
+    {id: 7, producto: "rubor en polvo tres colores", precio: 15000, stock: 6},
+    {id: 8, producto: "rubor en polvo dos colores", precio: 10000, stock: 0},
+    {id: 9, producto: "rubor en barra rosa claro", precio: 25000, stock: 3},
+    {id: 10, producto: "rubor en barra rosa oscuro", precio: 25000, stock: 0},
+    {id: 11, producto: "rubor en barra rosa marron oscuro", precio: 25000, stock: 1},
+    {id: 12, producto: "corrector de ojeras claro", precio: 20000, stock: 4},
+    {id: 13, producto: "corrector de ojeras medio", precio: 20000, stock: 0},
+    {id: 14, producto: "sombras para contornear rostro duo", precio: 16000, stock: 5},
+    {id: 15, producto: "iluminador", precio: 12000, stock: 8},
+    {id: 16, producto: "mascara para pestañas transparente", precio: 20000, stock: 14},
+]
 
+let ingresarProducto
+let cargarOtroProducto
+let ingresarPrecio
+let ingresarStock
+let generadorDeId = 16
+generadorDeId += 1
 
-function pedirDatos() {
-    do {
-        montoAhorro = Number(prompt("Monto objetivo de ahorro"))
-    } while (montoAhorro == "");
-
-    do {
-        cantidadTiempo = Number(prompt("Cantidad de meses para ahorrarlo"))
-    } while (montoAhorro == "");
+function ingresarProductoNuevo() {
+    do {  
+        ingresarProducto = prompt("Producto");
+        ingresarPrecio = parseInt(prompt("Precio"));
+        ingresarStock = parseInt(prompt("Stock"))
+        
+        productos.push({ id: generadorDeId, producto: ingresarProducto, precio: ingresarPrecio, stock: ingresarStock });
+        
+        cargarOtroProducto = prompt(
+        "Deseas cargar otro producto (SI/NO)"
+      ).toLocaleUpperCase();
+    } while (cargarOtroProducto != "NO");
 }
 
+function eliminarProductosConStock() {
+    for (const producto of productos) {
+       // let stockDelProducto = producto.stock
+       console.log(producto)
 
-function objetivoAhorro(monto, tiempo) {
-    return monto / tiempo
-}
+       if (producto.stock == 0){
 
-
-function resultado() {
-    let llamado = pedirDatos()
-    let ahorro = objetivoAhorro(montoAhorro, cantidadTiempo)
-    ahorro = ahorro.toFixed(0)
-
-    for (let i = 1; i <= cantidadTiempo; i++) {
-        console.log("El mes ", i, "tendras ahorrado ", ahorro * i)
+           productos.splice(producto.stock, 1);
+       }
+      //do {
+      //} while (productos.stock > 0);
     }
-
-    alert("Necesitas ahorrar " + ahorro + " durante " + cantidadTiempo + " meses para alcanzar tu objetivo de " + montoAhorro)
 }
 
-
-function recalcularAhorro() {
-    let recalcular
-
-    do {
-        recalcular = prompt("¿Deseas recalcular?. Escribe (SI/NO)")
-        recalcular = recalcular.toUpperCase()
-
-        if (recalcular == "SI") {
-            console.log("Recalculando...")
-            muestraResultado = resultado()
-        } else {
-            alert("Gracias por usar nuestra aplicacion")
-        }
-
-    } while (recalcular != "NO")
+function mostrarProductos() {
+    let guardar = "";
+    for (const producto of productos) {
+      guardar += "- " + producto.producto + "\n "
+    }
+    console.log(`Tienes ${productos.length} productos con Stock 0, detalle:\n ${guardar}`);
+    alert(`Tienes ${productos.length} productos con Stock 0, detalle:\n ${guardar}`);
 }
 
-let muestraResultado = resultado()
-let solicitaRecalcular = recalcularAhorro()
+ingresarProductoNuevo()
+
+eliminarProductosConStock()
+
+mostrarProductos()
+
+
